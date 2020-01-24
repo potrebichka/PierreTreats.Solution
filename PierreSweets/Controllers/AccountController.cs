935480baf1a97bfdc8ca03cece1d8256e8811101
Nina Potrebich <potrebich@gmail.com>
2020-01-24 10:47:57 -0800

    Add styling to account pages

--- PierreSweets/Controllers/AccountController.cs
+++ PierreSweets/Controllers/AccountController.cs
@@ -36,7 +36,7 @@ namespace PierreSweets.Controllers
             IdentityResult result = await _userManager.CreateAsync(user, model.Password);
             if (result.Succeeded)
             {
-                return RedirectToAction("Index");
+                return RedirectToAction("Index", "Home");
             }
             else
             {
