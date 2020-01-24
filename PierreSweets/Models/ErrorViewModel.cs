0dacf95c930e6e88cdec030a110abe4bd38df2e4
Nina Potrebich <potrebich@gmail.com>
2020-01-24 08:34:32 -0800

    Init project

---
+++ PierreSweets/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PierreSweets.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
