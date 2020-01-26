46107cdf42758aa4b63501f9ec50b4bd1f989b63
Nina Potrebich <potrebich@gmail.com>
2020-01-25 16:35:44 -0800

    Add IsSubmitted property to Order class. Update DB

---
+++ PierreSweets/Migrations/20200126003115_AddIsSubmittedToOrder.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PierreSweets.Migrations
+{
+    public partial class AddIsSubmittedToOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSubmitted",
+                table: "Orders",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSubmitted",
+                table: "Orders");
+        }
+    }
+}
--- PierreSweets/Models/Order.cs
+++ PierreSweets/Models/Order.cs
@@ -9,6 +9,7 @@ namespace PierreSweets.Models
     [Display(Name = "Order description")]
     public string Description { get; set; }
     public double Price { get; set; }
+    public bool IsSubmitted { get; set; }
     public virtual ApplicationUser User{ get; set; }
     public virtual List<OrderTreat> Treats {get;set;}
   }
